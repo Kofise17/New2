608487c3fe151933750fb72df7057f818d495901
Wout Vinckevleugel <s114220@ap.be>
2021-03-18 12:28:10 +0100

    heroku changes

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use('/', indexRouter);
 app.use('/home', homeRouter);
 
-app.listen(process.env.PORT || 3000, 
+/* app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running...")
-);
+); */
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

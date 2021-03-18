81b1e79d9e23dd7c9fe8d6ff6c9a00437aab40bb
Kofise17 <62119634+Kofise17@users.noreply.github.com>
2021-03-18 17:12:46 +0100

    removed get welcome page and get login page from index.js

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,4 @@ router.get('/', function(req, res, next) {
     res.redirect('/home');
 });
 
-/* SHOW LOGIN PAGE*/
-router.get('/login', (req, res) => {
-    res.render('login.ejs', {})
-})
-
-/* SHOW WELCOME PAGE */
-router.get('/welcome', (req, res) => {
-    res.render('welcome.ejs', {})
-})
-
 module.exports = router;

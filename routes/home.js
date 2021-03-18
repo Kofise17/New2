e902a29449baa7517e18e1025e6d73746e6ad588
Wout Vinckevleugel <s114220@ap.be>
2021-03-18 09:25:00 +0100

    I can now add data to the db

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 const MongoClient = require('mongodb').MongoClient
 const connstring = "mongodb+srv://Admin:Admin@cluster0.8cpdn.mongodb.net/test?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
+const client = new MongoClient(connstring);
 var db;
+var dbName = "SWS_DB";
 const functions = require("../public/js/functional1");
 const signups = require("../public/js/signup");
 
@@ -22,56 +24,49 @@ MongoClient.connect(connstring, {useUnifiedTopology: true}, (err, database) => {
 
   /* ADD USER TO DB*/
   router.post('/signup', (req, res) => {
-    if(signups.signup()){
-      createUser();
-      res.redirect('/home/welcome')
+    if(true){
+      //console.log(req);
+      var jsonData = {
+        "name": [req.body.name],
+        "firstname" : [req.body.firstname],
+        "adressline" : [req.body.adressline],
+        "username" : [req.body.username],
+        "email" : [req.body.email],
+        "password" : [req.body.password]
+      };
+      console.log(jsonData);
+      createUser(req).catch(console.dir);
+      res.redirect('/home/welcome');
     }
-    /* db.collection('Users').insertOne(req.body, (err, result) => {
-      if (err) return
-      res.redirect('/home/welcome')
-    }) */
   })
 
   /* SHOW WELCOME PAGE */
   router.get('/welcome', (req, res) => {
     res.render('welcome.ejs', {})
   })
-
-  /* SHOW ADD PRODUCT FORM */
-  router.get('/add', (req, res) => {
-    res.render('add.ejs', {})
-  })
-  router.post('/add', (req, res) => {
-    res.render('search_not_found.ejs', {})
-  })
 })
 
 module.exports = router;
 
 
-async function createUser() {
-  try{
-      // Use the collection "Users"
-      const col = db.collection("Users");
+async function createUser(req) {
+  //console.log(res);
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
+    const db = client.db(dbName);
 
-      var jsonData = {
-          "name": document.getElementById("name").value,
-          "firstName" : document.getElementById("firstname").value,
-          "adressLine" : document.getElementById("addressline").value,
-          "userName" : document.getElementById("username").value,
-          "email" : document.getElementById("email").value,
-          "password" : document.getElementById("password").value
-      };
-      console.log(jsonData);
-      // Insert a single document, wait for promise so we can read it back
-      const p = await col.insertOne(jsonData);
+    // Use the collection "people"
+    const col = db.collection("Users");
 
-      // Find one document
-      const myDoc = await col.findOne();
+    // Insert a single document, wait for promise so we can read it back
+    const p = await col.insertOne(jsonData);
 
-      // Print to the console
-      console.log(myDoc);
-  }catch(err){
-      console.error(err.stack);
+  } catch (err) {
+    console.log(err.stack);
+  }
+
+  finally {
+    await client.close();
   }
 }

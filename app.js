a9a551166ff8d10c3bc5420ff29ad6729a53718e
Wout Vinckevleugel <s114220@ap.be>
2021-03-18 12:26:05 +0100

    changes for heroku

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var createError = require('http-errors');
+
 var express = require('express');
+var app = express();
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var homeRouter = require('./routes/home');
-//var functions = require('./public/js/functional1.js');
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +24,10 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', indexRouter);
 app.use('/home', homeRouter);
+
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running...")
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

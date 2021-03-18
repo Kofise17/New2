6472f2905c3234ecb24c8292d30123720453593a
Wout Vinckevleugel <s114220@ap.be>
2021-03-18 14:19:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-var homeRouter = require('./routes/home.js');
+var homeRouter = require('./routes/home');
 //#endregion
 
 //#region app config

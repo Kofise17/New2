a5c3fccfa534bed41e284dbacac8d382ac957f0d
Kofise17 <62119634+Kofise17@users.noreply.github.com>
2021-03-18 17:11:23 +0100

    Undo the deletion of index.js(My Bad)

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.redirect('/home');
+});
+
+/* SHOW LOGIN PAGE*/
+router.get('/login', (req, res) => {
+    res.render('login.ejs', {})
+})
+
+/* SHOW WELCOME PAGE */
+router.get('/welcome', (req, res) => {
+    res.render('welcome.ejs', {})
+})
+
+module.exports = router;

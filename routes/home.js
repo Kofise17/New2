e9a55cd0f020e630dacd22ade8223f0375bfde8f
Wout Vinckevleugel <s114220@ap.be>
2021-03-20 11:09:29 +0100

    added router
    it was gone for some reason

--- routes/home.js
+++ routes/home.js
@@ -1,3 +1,5 @@
+var express = require('express');
+var router = express.Router();
 const MongoClient = require('mongodb').MongoClient
 const connstring = "mongodb+srv://Admin:Admin@cluster0.8cpdn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&useUnifiedTopology=true";
 const client = new MongoClient(connstring);

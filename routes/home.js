2652d84689236255764c287e6e3ff150750e15d0
Kofise17 <62119634+Kofise17@users.noreply.github.com>
2021-03-21 14:10:47 +0100

    Added an async function findUser

--- routes/home.js
+++ routes/home.js
@@ -8,6 +8,7 @@ const connstring = "mongodb+srv://Admin:Admin@cluster0.8cpdn.mongodb.net/myFirst
 const client = new MongoClient(connstring);
 const functions = require("../public/js/functional1");
 const signups = require("../public/js/signup");
+const login = require("../public/js.login");
 const BREACHED_PASSWORD_TEXT = "Your password must not be contained in the list of breached passwords";
 //#endregion
 
@@ -23,21 +24,42 @@ router.get('/signup', (req, res) => {
 
 /* SHOW WELCOME PAGE */
 router.get('/welcome', (req, res) => {
-  res.render('welcome.ejs', {});
+    res.render('welcome.ejs', {});
 })
 
 /* AFTER SIGNUP BUTTON PUSH */
 router.post('/signup', (req, res) => {
-  checkSignUpPlusCreateUser(req,res);
+    checkSignUpPlusCreateUser(req, res);
 })
 
+/*FIND USER IN DB*/
+async function findUser() {
+    try {
 
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+
+        // Use the collection "USers"
+        const col = db.collection("Users");
+        col.findOne({
+            $and: [
+                { username: [req.body.username] },
+                { password: login.SHA1([req.boy.password]) }
+            ]
+        })
+    } catch (error) {
+        console.log(error.stack)
+    } finally {
+        await client.close();
+    }
+}
 /* ADD USER TO DB */
 async function createUser(jsonData) {
-  try {
-    await client.connect(); // Connect to the Mongoclient
-    console.log("Connected correctly to server");
-    const db = client.db(dbName);
+    try {
+        await client.connect(); // Connect to the Mongoclient
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
 
         // Use the collection "Users"
         const col = db.collection("Users");
@@ -45,45 +67,44 @@ async function createUser(jsonData) {
         // Insert a single document, wait for promise so we can read it back if desired
         const p = await col.insertOne(jsonData);
 
-  } catch (err) {
-    console.log(err.stack); // Logging error if something were to go wrong
-  }
-
-  finally {
-    await client.close(); // Close the client
-  }
+    } catch (err) {
+        console.log(err.stack); // Logging error if something were to go wrong
+    } finally {
+        await client.close(); // Close the client
+    }
 }
 
 /* CHECK IF THE SIGNUP INFO IS POSSIBLE */
-async function checkSignUpPlusCreateUser(req, res){
-  /**
-   * Calling the signup function to see if password is alright
-   * @returns a promise so we wait for the "then" to complete and give the boolean back
-   */
-  var signupIsOk = await signups.signup([req.body.password]).then((response) => {return response;})
-  .catch(error => console.error('On get API Answer'+ error, error));
+async function checkSignUpPlusCreateUser(req, res) {
+    /**
+     * Calling the signup function to see if password is alright
+     * @returns a promise so we wait for the "then" to complete and give the boolean back
+     */
+    var signupIsOk = await signups.signup([req.body.password]).then((response) => { return response; })
+        .catch(error => console.error('On get API Answer' + error, error));
 
-  // If the password is alright go further
-  if(signupIsOk){
-    console.log("3.    signup = true (succeeded)");
+    // If the password is alright go further
+    if (signupIsOk) {
+        console.log("3.    signup = true (succeeded)");
 
-    // Set up the jsonData to send to createuser
-    var jsonData = {
-      "name": [req.body.name].toString(),
-      "firstname" : [req.body.firstname].toString(),
-      "adressline" : [req.body.adressline].toString(),
-      "username" : [req.body.username].toString(),
-      "email" : [req.body.email].toString(),
-      "password" : signups.SHA1([req.body.password])
-    };
+        // Set up the jsonData to send to createuser
+        var jsonData = {
+            "name": [req.body.name].toString(),
+            "firstname": [req.body.firstname].toString(),
+            "adressline": [req.body.adressline].toString(),
+            "username": [req.body.username].toString(),
+            "email": [req.body.email].toString(),
+            "password": signups.SHA1([req.body.password])
+        };
 
-    createUser(jsonData).catch(console.dir); // CreateUser
-    res.redirect('/home/welcome'); // Redirect to welcome page
-  }else{
-    console.log("3.    signup = false (something went wrong)");
-    res.render('signup.ejs', {'errorInfo': BREACHED_PASSWORD_TEXT}); // Redirect to signup page on error
-  }
-  return;
+        createUser(jsonData).catch(console.dir); // CreateUser
+        res.redirect('/home/welcome'); // Redirect to welcome page
+    } else {
+        console.log("3.    signup = false (something went wrong)");
+        res.render('signup.ejs', { 'errorInfo': BREACHED_PASSWORD_TEXT }); // Redirect to signup page on error
+    }
+    return;
 }
 
+module.exports = router;
 module.exports = router;

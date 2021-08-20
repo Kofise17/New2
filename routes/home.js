fc9600c0e9449bfe5bfdc5b333283c6deaea1a3b
Kofise17 <62119634+Kofise17@users.noreply.github.com>
2021-08-20 07:46:40 +0200

    Attempt to fix Login
    Tried multiple ways to fix the login screen but it wouldn't work

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,11 @@
 //#region Variables and Constants
 var express = require('express');
 var router = express.Router();
-var dbName = "SWS_DB";
+var dbName = "securitySite";
+
 
 const MongoClient = require('mongodb').MongoClient
-const connstring = "mongodb+srv://Admin:nktnsXEWabP5M4y@cluster0.8cpdn.mongodb.net/Cluster0?retryWrites=true&w=majority&useUnifiedTopology=true";
+const connstring = "mongodb+srv://securitySiteAdmin:HtvT6h1782xGEhzw@cluster0.qrmeb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&useUnifiedTopology=true";
 const client = new MongoClient(connstring);
 const signups = require("../public/js/signup");
 const BREACHED_PASSWORD_TEXT = "Your password must not be contained in the list of breached passwords";
@@ -35,9 +36,9 @@ router.post('/signup', (req, res) => {
 
 /* AFTER LOGIN BUTTON PUSH */
 router.post('/welcome', (req, res) => {
-    login(req, res);
-})
-//#endregion
+        login(req, res);
+    })
+    //#endregion
 
 //#region Extra functions
 /**
@@ -48,22 +49,20 @@ router.post('/welcome', (req, res) => {
  */
 async function login(req, res) {
     username = [req.body.username].toString();
-    password = crypto.createHash([req.body.password].toString());
+    password = signups.SHA1([req.body.password].toString());
     var loginIsOK = await findUser(username, password).then(
-		(response) => 
-		{
-			//console.log(loginIsOK);
-			if (loginIsOK !== null) {
-				console.log("Log in succeeded");
-				res.redirect('/home/welcome');
-			} else {
-				console.log("3.    login = false (something went wrong)");
-				res.render('login.ejs', {}); // Redirect to login page on error
-			}
-			return response; 
-		}).catch(
-			error => console.error('On get API Answer' + error, error)
-		);
+        (response) => {
+            if (loginIsOK !== null) {
+                console.log("Log in succeeded");
+                res.redirect('/home/welcome');
+            } else {
+                console.log("3.    login = false (something went wrong)");
+                res.render('login.ejs', {}); // Redirect to login page on error
+            }
+            return response;
+        }).catch(
+        error => console.error('On get API Answer' + error, error)
+    );
 }
 
 /**
@@ -80,7 +79,7 @@ async function findUser(username, password) {
         console.log("Connected correctly to server");
         const db = client.db(dbName);
         // Use the collection "Users"
-        const col = db.collection("Users");
+        const col = db.collection("users");
         found = await col.findOne({
             $and: [{
                 "username": username
@@ -88,12 +87,12 @@ async function findUser(username, password) {
                 "password": password
             }]
         })
-        //console.log(found);
+        console.log("Found user", found);
     } catch (error) {
         console.log("login failed");
         //console.log(error.stack);
     } finally {
-        //await client.close();
+        await client.close();
         console.log("Client has closed");
     }
 }
@@ -109,7 +108,7 @@ async function createUser(jsonData) {
         const db = client.db(dbName);
 
         // Use the collection "Users"
-        const col = db.collection("Users");
+        const col = db.collection("users");
 
         // Insert a single document, wait for promise so we can read it back if desired
         const p = await col.insertOne(jsonData);
@@ -129,29 +128,29 @@ async function createUser(jsonData) {
  */
 async function checkSignUpPlusCreateUser(req, res) {
     var signupIsOk = await signups.signup([req.body.password]).then(
-		(response) => { 
-			if (response) {
-				console.log("3.    signup = true (succeeded)");
-		
-				// Set up the jsonData to send to createuser
-				var jsonData = {
-					"name": [req.body.name].toString(),
-					"firstname": [req.body.firstname].toString(),
-					"adressline": [req.body.adressline].toString(),
-					"username": [req.body.username].toString(),
-					"email": [req.body.email].toString(),
-					"password": crypto.createHash([req.body.password].toString())
-				};
-		
-				createUser(jsonData).catch(console.dir); // CreateUser
-				res.redirect('/home/welcome'); // Redirect to welcome page
-			} else {
-				console.log("3.    signup = false (something went wrong)");
-				res.render('signup.ejs', { 'errorInfo': BREACHED_PASSWORD_TEXT }); // Redirect to signup page on error
-			}
-			return response; 
-		}
-	)
+            (response) => {
+                if (response) {
+                    console.log("3.    signup = true (succeeded)");
+
+                    // Set up the jsonData to send to createuser
+                    var jsonData = {
+                        "name": [req.body.name].toString(),
+                        "firstname": [req.body.firstname].toString(),
+                        "adressline": [req.body.adressline].toString(),
+                        "username": [req.body.username].toString(),
+                        "email": [req.body.email].toString(),
+                        "password": signups.SHA1([req.body.password].toString())
+                    };
+
+                    createUser(jsonData).catch(console.dir); // CreateUser
+                    res.redirect('/home/welcome'); // Redirect to welcome page
+                } else {
+                    console.log("3.    signup = false (something went wrong)");
+                    res.render('signup.ejs', { 'errorInfo': BREACHED_PASSWORD_TEXT }); // Redirect to signup page on error
+                }
+                return response;
+            }
+        )
         .catch(error => console.error('On get API Answer' + error, error));
 }
 //#endregion
